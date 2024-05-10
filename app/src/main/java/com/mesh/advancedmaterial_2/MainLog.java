4fe506caa7d52a6dd87c2686dcfbc508dd28c919
Mesh Anthony <96480799+Mesh-Sys@users.noreply.github.com>
2024-05-10 10:36:52 +0100

    Delete app directory

--- app/src/main/java/com/mesh/advancedmaterial_2/MainLog.java
+++
@@ -1,9 +0,0 @@
-package com.mesh.advancedmaterial_2;
-
-public class MainLog {
-    public MainLog() {
-    }
-    public static final String DEBUG_TAG = "MESH_DEBUG";
-    public static final String INFO_TAG = "MESH_INFO";
-    public static final String ERROR_TAG = "MESH_ERROR";
-}
